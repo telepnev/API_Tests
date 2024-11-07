dc6ea0b6b692f33b9ee8bfe95538da1753fa52d2
Evgeny Telepnev <telepnef@yandex.ru>
2024-11-07 20:27:54 +0300

    Initial commit

---
+++ src/test/java/api/SimpleApiTests.java
@@ -0,0 +1,12 @@
+package api;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class SimpleApiTests {
+    @Test
+    public void getAllUsersTest() {
+
+    }
+}
