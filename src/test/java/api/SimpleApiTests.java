d73005c559d5ee2aacedb12dd758b37f69973845
Telepnev Evgeny <telepnef@gmail.com>
2024-11-08 12:14:54 +0300

    addNewUserTest()

--- src/test/java/api/SimpleApiTests.java
+++ src/test/java/api/SimpleApiTests.java
@@ -1,6 +1,17 @@
 package api;
 
+import io.restassured.response.Response;
+import model.Address;
+import model.Geolocation;
+import model.Name;
+import model.UserRoot;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -35,9 +46,64 @@ public class SimpleApiTests {
         int limitSize = 3;
 
         given().queryParam("limit", limitSize)
-                .get("https://fakestoreapi.com/users/")
+                .get("https://fakestoreapi.com/users")
                 .then()
                 .log().body()
-                .body("", hasSize(limitSize));
+                // .body("", hasSize(limitSize))
+                .body("", hasSize(greaterThanOrEqualTo(limitSize)));
+    }
+
+    // проверка сортировки
+    @Test
+    public void getAllUsersSortByDescTest() {
+        String sortType = "desc";
+
+        Response sortedResponse = given().queryParam("sort", sortType)
+                .get("https://fakestoreapi.com/users")
+                .then()
+                .extract().response();
+
+
+        Response notSortedResponse = given().get("https://fakestoreapi.com/users")
+                .then()
+                .extract().response();
+
+        List<Integer> sortedResponseIds = sortedResponse.jsonPath().getList("id");
+        List<Integer> notSortedResponseIds = notSortedResponse.jsonPath().getList("id");
+
+        List<Integer> sortedByCodeList = notSortedResponseIds
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .toList();
+
+        Assertions.assertEquals(sortedByCodeList, sortedResponseIds);
+    }
+
+    @Test
+    public void addNewUserTest(){
+        Name name = new Name("Geka", "Telepnev");
+        Geolocation geolocation = new Geolocation("-37.3159", "81.1496");
+        Address address = Address.builder()
+                .city("Voroneg")
+                .street("Wall street")
+                .number(666)
+                .zipcode("12926-3874")
+                .geolocation(geolocation)
+                .build();
+
+        UserRoot bodyRequest = UserRoot.builder()
+                .email("mail@maol.ru")
+                .username("GekaT")
+                .password("12345567")
+                .name(name)
+                .address(address)
+                .phone("+98877923424234")
+                .build();
+
+        given().body(bodyRequest)
+                .post("https://fakestoreapi.com/users")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", notNullValue());
     }
 }
